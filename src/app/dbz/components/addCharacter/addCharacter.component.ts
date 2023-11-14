ff2e33ae529730d836519fd3de5a431f570fd2da
fer8707 <fer8707@gmail.com>
2023-11-14 17:50:40 -0600

    preprod

---
+++ src/app/dbz/components/addCharacter/addCharacter.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'app-dbz-add-character',
+  templateUrl: './addCharacter.component.html',
+  styleUrls: ['./addCharacter.component.css']
+})
+export class AddCharacterComponent {
+
+  @Output()
+  onNewCharacter: EventEmitter <Character> = new EventEmitter();
+
+  public character: Character= {
+    name:'',
+    power: 0
+  };
+
+  emitCharacter(): void {
+    console.log(this.character);
+    this.onNewCharacter.emit(this.character);
+    this.character = { name: '', power: 0 };
+  }
+
+}
